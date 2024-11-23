2a4df86540ee538305dc2e574eb18a001558d2ee
Riley Kneen-Teed <kneenrt@gmail.com>
2024-11-22 21:24:18 -0400

    initalized

---
+++ src/app.js
@@ -0,0 +1,21 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const path = require('path');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use('/public', express.static(path.join(__dirname, '../public')));
+
+app.get('/', (req, res) => {
+  res.send('API is running...');
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
